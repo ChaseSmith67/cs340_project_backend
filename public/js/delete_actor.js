c8498a047d04790494fd8f539cb934c212853031
ChaseSmith67 <smitcha6@oregonstate.edu>
2023-03-03 01:29:14 -0600

    implement delete for actors

--- public/js/delete_actor.js
+++ public/js/delete_actor.js
@@ -1,3 +1,4 @@
+
 function deleteActor(actor_id) {
     let link = '/delete-actor-ajax/';
     let data = {
@@ -8,21 +9,22 @@ function deleteActor(actor_id) {
       url: link,
       type: 'DELETE',
       data: JSON.stringify(data),
-      contentType: "application/json; charset=utf-8",
+      contentType: "application/json; charset=utf-8", 
       success: function(result) {
         deleteRow(actor_id);
       }
     });
   }
-  
-  function deleteRow(actor_id){
-      let table = document.getElementById("people-table");
-      for (let i = 0, row; row = table.rows[i]; i++) {
-         if (table.rows[i].getAttribute("data-value") == actor_id) {
-              table.deleteRow(i);
-              break;
-         }
-      }
-  }
 
-// export default function deleteActor(actor_id);
+  function deleteRow(actor_id){
+
+    let table = document.getElementById("people-table");
+    for (let i = 0, row; row = table.rows[i]; i++) {
+       //iterate through rows
+       //rows would be accessed using the "row" variable assigned in the for loop
+       if (table.rows[i].getAttribute("data-value") == actor_id) {
+            table.deleteRow(i);
+            break;
+       }
+    }
+}
