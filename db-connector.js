bf63d54532d6c8d57d0cdd9a04d600642495dc8c
ChaseSmith67 <smitcha6@oregonstate.edu>
2023-03-21 00:45:55 -0500

    remove db data

--- db-connector.js
+++ db-connector.js
@@ -5,10 +5,10 @@ const mysql = require('mysql');
 // Create a 'connection pool' using the provided credentials
 var pool = mysql.createPool({
     connectionLimit : 10,
-    host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_smitcha6',
-    password        : '7055',
-    database        : 'cs340_smitcha6'
+    host            : '',
+    user            : '',
+    password        : '',
+    database        : ''
 })
 
 // Export it for use in our application
